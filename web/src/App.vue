26ae39463d2c9d30de01060781f442971345fb70
M-Garrigues <mathieu.garrigues@etu.univ-lyon1.fr>
2019-12-08 17:42:56 +0100

    Created MainWrapper and Footer Vue components

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
     <Navbar></Navbar>
+    <MainWrapper></MainWrapper>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar";
+import MainWrapper from "./components/MainWrapper";
+import Footer from "./components/Footer";
 
 export default {
   name: 'app',
   components: {
+    Footer,
+    MainWrapper,
     Navbar
   }
 }
---
+++ web/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="stamp" class="container">
+        <div class="hexagon"></div>
+        <div id="copyright" class="container">
+            <p>&copy; PLP. All rights reserved. | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
