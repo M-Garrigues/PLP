e90893c9ed86d2a817f4320c4e3361882605a606
M-Garrigues <mathieu.garrigues@etu.univ-lyon1.fr>
2019-12-09 14:27:39 +0100

    Created Match.vue component

---
+++ web/src/components/in-wrapper/Match.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="row">
+        <div id="content">
+            <h3>{{H_team}}  --- {{A_team}}</h3>
+            <p> {{H_cote}} - {{D_cote}} - {{A_cote}}</p>
+            <p>{{league}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Match",
+        props: [
+            "id",
+            "H_team",
+            "A_team",
+            "H_cote",
+            "D_cote",
+            "A_cote",
+            "league"
+        ]
+    }
+</script>
+
+<style scoped>
+
+</style>
--- web/src/components/in-wrapper/MatchList.vue
+++ web/src/components/in-wrapper/MatchList.vue
@@ -6,39 +6,114 @@
             </div>
         </div>
 
-            <div class="ebox1">
-                <div class="title">
-                    <h3>Fusce ultrices fringilla</h3>
-                </div>
-                <ul>
-                    <li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>
-                    <li>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>
-                    <li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
-                    <li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>
-                </ul>
-
-            </div>
-
-            <div class="ebox2">
-                <div class="title">
-                    <h3>Donec dictum metus</h3>
-                </div>
-                <ul>
-                    <li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>
-                    <li>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>
-                    <li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
-                    <li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>
-                </ul>
-
-            </div>
-
+        <Match v-for="match of matches" :key="match.id" :H_team="match.H_team" :A_team="match.A_team" :H_cote="match.H_cote"
+              :D_cote="match.D_cote" :A_cote="match.A_cote" :league="match.league"></Match>
     </div>
 </template>
 
 <script>
+    import Match from "./Match";
     export default {
-        name: "MatchList"
+        name: "MatchList",
+        components: {Match},
+        data() {
+            return {
+                matches: [
+                    {
+                        id : 1,
+                        H_team : "PSG",
+                        A_team : "OL",
+                        H_cote : 1.56,
+                        D_cote : 3.38,
+                        A_cote : 6.09,
+                        league : "ligue1"
+                    },
+                    {
+                        id : 2,
+                        H_team : "Reims",
+                        A_team : "OM",
+                        H_cote : 2.46,
+                        D_cote : 5.48,
+                        A_cote : 1.99,
+                        league : "ligue1"
+                    },
+                    {
+                        id : 3,
+                        H_team : "Nîmes",
+                        A_team : "Saint-Etienne",
+                        H_cote : 4.87,
+                        D_cote : 2.32,
+                        A_cote : 2.94,
+                        league : "ligue1"
+                    },
+                    {
+                        id : 4,
+                        H_team : "Liverpool",
+                        A_team : "Bournemouth",
+                        H_cote : 1.56,
+                        D_cote : 3.38,
+                        A_cote : 6.09,
+                        league : "premierLeague"
+                    },
+                    {
+                        id : 5,
+                        H_team : "Bayern Munich",
+                        A_team : "RB Leipzig",
+                        H_cote : 1.78,
+                        D_cote : 3.27,
+                        A_cote : 6.10,
+                        league : "bundesliga"
+                    },
+                    {
+                        id : 6,
+                        H_team : "Juve",
+                        A_team : "Bologna",
+                        H_cote : 6.01,
+                        D_cote : 2.19,
+                        A_cote : 9.10,
+                        league : "serieA"
+                    },
+                    {
+                        id : 7,
+                        H_team : "Fiorentina",
+                        A_team : "AC Milan",
+                        H_cote : 4.81,
+                        D_cote : 1.12,
+                        A_cote : 7.04,
+                        league : "serieA"
+                    },
+                    {
+                        id : 8,
+                        H_team : "FC Barcelona",
+                        A_team : "Eibar",
+                        H_cote : 1.25,
+                        D_cote : 4.78,
+                        A_cote : 10.29,
+                        league : "liga"
+                    },
+                    {
+                        id : 9,
+                        H_team : "Valencia",
+                        A_team : "Real Madrid",
+                        H_cote : 4.32,
+                        D_cote : 3.82,
+                        A_cote : 2.41,
+                        league :"liga"
+                    },
+                    {
+                        id : 10,
+                        H_team : "Bordeaux",
+                        A_team : "Rennes",
+                        H_cote : 2.54,
+                        D_cote : 3.87,
+                        A_cote : 2.45,
+                        league : "ligue1"
+                    }
+                ]
+            }
+        }
     }
+
 </script>
 
 <style scoped>
