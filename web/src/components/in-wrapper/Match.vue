77636d1a57ab9e177d627549da36b1fff7d0ad5b
Hugo <hugo.marguet@hotmail.fr>
2019-12-11 16:42:27 +0100

    matches vue updated with indicator

--- web/src/components/in-wrapper/Match.vue
+++ web/src/components/in-wrapper/Match.vue
@@ -1,9 +1,30 @@
 <template>
     <div id="row">
         <div id="content">
-            <h3>{{H_team}}  --- {{A_team}}</h3>
-            <p> {{H_cote}} - {{D_cote}} - {{A_cote}}</p>
-            <p>{{league}}</p>
+            <table align="center" width=65%>
+                <tr>
+                    <th width=35% >{{H_team}}</th>
+                    <th width=10%>1</th>
+                    <th width=10%>N</th>
+                    <th width=10%>2</th>
+                    <th width=35%>{{A_team}}</th>
+                </tr>
+                <tr>
+                    <td/>
+                    <td>{{H_cote}}</td>
+                    <td>{{D_cote}}</td>
+                    <td>{{A_cote}}</td>
+                    <td/>
+                </tr>
+                <tr>
+                    <td/>
+                    <td><div class="hexagonLittle"><span>{{H_ind}}</span></div></td>
+                    <td><div class="hexagonLittle"><span>{{D_ind}}</span></div></td>
+                    <td><div class="hexagonLittle"><span>{{A_ind}}</span></div></td>
+                    <td/>
+                </tr>
+            </table>
+            <p><i>{{league}}</i></p>
         </div>
     </div>
 </template>
