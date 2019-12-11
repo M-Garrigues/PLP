1e5c028a6beb8ff47c0b1540dc356a5254388c43
M-Garrigues <mathieu.garrigues@etu.univ-lyon1.fr>
2019-12-11 17:18:02 +0100

    Added mongodb and a quick flask API for incoming matches

---
+++ etc/flask/server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+app = Flask(__name__)
+
+@app.route('/api')
+def api():
+ client = MongoClient("mongodb://plp_mongodb:27017")
+ return dumps(client.test_matches.matches.find())
+
+if __name__ == '__main__':
+ app.run(debug=True)
