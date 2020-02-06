3e899c7ef375b63c9753613f2f9fb996746712c3
M-Garrigues <mathieu.garrigues@etu.univ-lyon1.fr>
2020-02-06 16:36:36 +0100

    Bootstrap added, navbar now flexible

--- web/src/components/in-wrapper/MatchList.vue
+++ web/src/components/in-wrapper/MatchList.vue
@@ -6,7 +6,9 @@
             </div>
         </div>
         <div v-if="loading">
-            <h1>Chargement ...</h1>
+            <div>
+                <b-spinner style="width: 3rem; height: 3rem;" variant="warning" label="Large Spinner"></b-spinner>
+            </div>
         </div>
         <Match v-for="match of filteredMatches" :key="match.id" :H_team="match.H_team" :A_team="match.A_team" :H_cote="match.H_cote"
                :H_ind="match.H_ind" :D_cote="match.D_cote" :D_ind="match.D_ind" :A_cote="match.A_cote" :A_ind="match.A_ind" :league="match.league"></Match>
--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 require("./assets/template/fonts.css");
 require("./assets/template/main.css");
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = true;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   render: h => h(App),
