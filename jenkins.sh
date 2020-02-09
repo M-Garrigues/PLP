f9cecbe2a61367d35340d8d545b2ccded2ecb12d
Hugo <hugo.marguet@hotmail.fr>
2020-02-09 15:00:11 +0100

    update with hyperlink

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "Jenkins :"
-echo "  broswe : $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' plp_jenkins):8080"
-echo "  Admin password : $(docker exec -it plp_jenkins cat /var/jenkins_home/secrets/initialAdminPassword)"
+echo "Jenkins : http://$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' plp_jenkins):8080"
+echo "Admin password : $(docker exec -it plp_jenkins cat /var/jenkins_home/secrets/initialAdminPassword)"
 
